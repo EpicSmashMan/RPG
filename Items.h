3cb8cf073d73ab71babde97e069a3b3c7b9f96a2
EpicSmashMan <64053346+EpicSmashMan@users.noreply.github.com>
2020-06-09 12:57:19 -0700

    Add files via upload

--- Items.h
+++ Items.h
@@ -6,27 +6,58 @@
 
 	Felix Wallace-Tarry DO NOT STEAL (without permission)
 */
-#include<conio.h>
 #include<string>
 #include<vector>
 #include<time.h>
 
 using namespace std;
 
+class Element {
+public:
+	string name;
+	int rolSize;
+	Element(string name, int rolSize);
+};
 
-vector<string> typeList;
+Element::Element(string _name, int _rolSize) {
+	name = _name;
+	rolSize = _rolSize;
+}
+
+vector<Element> elmntList;
+
+class Type {
+public:
+	string name;
+	int attBonus;
+	int rolMulti;
+	Type(string name, int attBonus, int rolMulti);
+};
+
+Type::Type(string _name, int _attBonus, int _rolMulti) {
+	name = _name;
+	attBonus = _attBonus;
+	rolMulti = _rolMulti;
+};
+
+
+vector<Type> typeList;
 
 class Weapon {
 	int attBonus, rolMulti, rolSize;
-	int magic;
 	int rolDam();
+	void randomize();
 	void findType(string);
+	void findElmnt(string);
 public:
-	string name, type;
+	string name, type, elmnt;
+	int magic, durab;
 	int rolAtt(int);
-	Weapon(string _type, int _magic);
-	Weapon(int _magic) : Weapon(string "-1", int _magic);
-	Weapon(string _type) : Weapon(string _type, int -1);
+	Weapon(string _type, int _magic, string _elmnt);
+	Weapon(int _magic) : Weapon("-1", _magic, "-1") {};
+	Weapon(string _type) : Weapon(_type, -1, "-1") {};
+	Weapon(string _type, string _elmnt) : Weapon(_type, -1, _elmnt) {};
+	Weapon(void) : Weapon("-1", -1, "-1") {};
 };
 
 /*
@@ -35,43 +66,92 @@ Object Constructors (Weapon)
 
 */
 
-Weapon::Weapon(string _type, int _magic) {
+
+Weapon::Weapon(string _type, int _magic, string _elmnt) {
+	randomize();
 	if (_magic == -1) {
-		magic = (rand() / 10000) % 100;
-	} else
+		if (rand() % 2 == 1) {
+			magic = rand() % 100;
+		}
+		else
+			magic = -2;
+	} 
+	else
 		magic = _magic;
-	if (type == "-1") {
-		type = typeList[(rand() / 1000) % typeList.size()];
-	} else
+	if (_type == "-1") {
+		int ran = rand() % typeList.size();
+		type = typeList[ran].name;
+		attBonus = typeList[ran].attBonus;
+		rolMulti = typeList[ran].rolMulti;
+	} 
+	else
 		findType(_type);
+	if (_elmnt == "-1") {
+		int ran = rand() % elmntList.size();
+		elmnt = elmntList[ran].name;
+		rolSize = elmntList[ran].rolSize;
+	} 
+	else
+		findElmnt(_elmnt);
+
 }
 
-
-/* Object Functions */
+/* Weapon Functions */
 
 int Weapon::rolAtt(int AC) {
 	int Dam;
-	if (10 + attBonus > AC)
+	int x = rand() % 20;
+	cout << "You attacked with a roll of " << x + attBonus << " (" << x << ")" << endl;
+	if (x + attBonus > AC)
 		Dam = rolDam();
+	else
+		Dam = 0;
+	cout << "You did " << Dam << " Damage" << endl;
 	return Dam;
 }
 
 int Weapon::rolDam() {
 	int Dam = 0;
-	for (int i = 0; i < rolMulti; i++)
-		Dam += rand() % rolSize + 1;
+	for (int i = 0; i < rolMulti; i++) {
+		Dam += (rand() % rolSize) + 1;
+	}
 	return Dam;
+}
+
+void Weapon::randomize() {
+	static bool seeded = false;
+	if (!seeded) {
+		srand(time(NULL));
+		seeded = true;
+	}
 }
 
 void Weapon::findType(string _type)
 {
 	for (int i = 0; i < typeList.size(); i++) {
-		if (_type == typeList[i]) {
-			this->type = typeList[i];
+		if (_type == typeList[i].name) {
+			type = typeList[i].name;
+			attBonus = typeList[i].attBonus;
+			rolMulti = typeList[i].rolMulti;
 			break;
 		}
-		else if (i == typeList.size() - 1) {
+		if (i == typeList.size() - 1) {
 			cout << "WEAPON TYPE NOT DEFINED" << endl;
+			abort();
+		}
+	}
+}
+
+void Weapon::findElmnt(string _elmnt)
+{
+	for (int i = 0; i < typeList.size(); i++) {
+		if (_elmnt == elmntList[i].name) {
+			elmnt = elmntList[i].name;
+			rolSize = elmntList[i].rolSize;
+			break;
+		}
+		if (i == elmntList.size() - 1) {
+			cout << "MATERIAL TYPE NOT DEFINED" << endl;
 			abort();
 		}
 	}
--- RPG.cpp
+++ RPG.cpp
@@ -2,8 +2,10 @@
 #include<conio.h>
 #include<string>
 #include<H:\Line.h>
+#include<numeric>
 #include<time.h>
 #include"Items.h"
+#include"Definitions.h"
 
 using namespace std;
 
@@ -12,13 +14,38 @@ using namespace std;
 int main() {
 	system("cls");
 	srand(time(NULL));
-	typeList.push_back("Mace");
-	typeList.push_back("Sword");
-	typeList.push_back("Axe");
-	typeList.push_back("Knife");
-	typeList.push_back("Hammer");
+	Type Hammer("Hammer", 4, 5);
+	Type Mace("Mace", 5, 4);
+	Type Axe("Axe", 6, 3);
+	Type Sword("Sword", 7, 2);
+	Type Knife("Knife", 10, 1);
+	typeList.push_back(Mace);
+	typeList.push_back(Sword);
+	typeList.push_back(Axe);
+	typeList.push_back(Knife);
+	typeList.push_back(Hammer);
+	Element Bronze("Bronze", 4);
+	Element Iron("Iron", 6);
+	Element Steel("Steel", 8);
+	Element Mythril("Mythril", 10);
+	Element Adamantium("Adamantium", 12);
+	elmntList.push_back(Bronze);
+	elmntList.push_back(Iron);
+	elmntList.push_back(Steel);
+	elmntList.push_back(Mythril);
+	elmntList.push_back(Adamantium);
 
-	Weapon weapon1("Knife");
-	cout << weapon1.type << endl;
+	Weapon weapon1("Sword", -2, "Bronze");
+	vector<int> damage;
+	int iter = 10000;
+	for (int x = 0; x < iter; x++) {
+		damage.push_back(weapon1.rolAtt(12));
+	}
+	
+	float average = accumulate(damage.begin(), damage.end(), 0.0) / damage.size();
+	int zeroes = count(damage.begin(), damage.end(), 0);
+	cout << "The average damage was " << average << endl;
+	cout << "The hit rate was " << (1-(double(zeroes)/iter))*100 << "%" << endl;
+
 	return 0;
 }
